2edc1e4287a012d6209f4b90fb92d50fc1a2fcac
NataliiaProkopenko87 <nataliiaprokopenko@yahoo.com>
2024-04-26 18:23:38 +0200

    Added JS to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("Hello World");
+function generatePoem(event) {
+    event.preventDefault();
+
+    new Typewriter('#poem', {
+        strings: "Liść pożółkły spada z drzewa",
+        autoStart: true,
+        delay: 1,
+        cursor:"",
+      });
+
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit",generatePoem);
