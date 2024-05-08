88ecdbb7b29f16849a71f455a711dbf7f104e85a
NataliiaProkopenko87 <nataliiaprokopenko@yahoo.com>
2024-05-08 12:00:07 +0200

    Remove consol.log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-    console.log("poem generated");
     new Typewriter('#poem', {
         strings: response.data.answer,
         autoStart: true,
@@ -20,10 +19,7 @@ function generatePoem(event) {
     let poemElement = document.querySelector("#poem");
     poemElement.classList.remove("hidden");
     poemElement.innerHTML = `<div class="generating">⏳Generating poem a Polish poem about ${instructionsInput.value}</div>`;
-    console.log("generating poem");
-    console.log(`Prompt: ${prompt}`);
-    console.log(`Context: ${context}`);
-
+    
     axios.get(apiUrl).then(displayPoem);
     
 
